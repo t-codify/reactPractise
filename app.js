c6d4c5a9fbe76a7fdcbdb82e1b9686096e61af65
Tanvi Nigam <tnigam06@gmail.com>
2024-06-27 16:56:39 +0530

    basic concepts on components and jsx

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
-const heading = React.createElement('div', { id: 'parent', xyz: 'abc' }, [
-	React.createElement('div', { id: 'child1' }, [
-		React.createElement('h1', {}, 'header1'),
-		React.createElement('h2', {}, 'header1'),
-	]),
-	React.createElement('div', { id: 'child2' }, [
-		React.createElement('h1', {}, 'header3'),
-		React.createElement('h2', {}, 'header4'),
-	]),
-]);
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+//React Element
+const heading = React.createElement(
+  "h1",
+  { id: "header" },
+  "Namaste React! from React element"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const userName = "user";
+//JSX- not html in javascript, instead its html like syntax
+const Title = () => {
+  return (
+    <h1 className="head" tabIndex="1">
+      Namaste React title Component composition
+    </h1>
+  );
+};
+//Compoennet composition- component used inside another like tittle in the following component
+//React Component
+//functional Component-
+const HeadingComponent = () => {
+  return (
+    <div id="container">
+      <Title />
+      {heading}
+      <h1 className="heading" tabIndex="2">
+        Welcome {userName} to Namaste React from Functional Component!
+      </h1>
+    </div>
+  );
+};
+
+root.render(<HeadingComponent />);
